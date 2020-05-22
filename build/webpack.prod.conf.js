d624cf17485a00521c764e941bbe671092a1d48d
cinderzoe <635962921@qq.com>
2020-05-22 16:42:03 +0800

    添加icon图标

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -69,6 +69,7 @@ const webpackConfig = merge(baseWebpackConfig, {
         ? 'index.html'
         : config.build.index,
       template: 'index.html',
+      favicon:'./favicon.ico',
       inject: true,
       minify: {
         removeComments: true,
